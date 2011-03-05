df857eb987a0219f87ec83695841fa0458889dd4
vipulkrsingh <vipulkrsingh@gmail.com>
2011-03-05 22:02:58 +0530

    Removed some unusued files

--- status.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-// show statuses
-$statuses = $facebook->api('/me/statuses');
-foreach($statuses['data'] as $status)
-{
-	echo $status["message"], "<br />";
-}
--- status_add.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config.php';
-
-// add a status message
-$status = $facebook->api('/me/feed', 'POST', array('message' => 'This post came from my app.'));
-
-var_dump($status);
